79387803847d5067ea1e9e8ac2fccd35ef50c1f8
Serik <serik_siti@mail.ru>
2017-09-13 11:47:03 +0600

    Using Autofac

---
+++ Messager/EmailSender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messager
+{
+    class EmailSender : IMessageSender
+    {
+        public void SendMessage()
+        {
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress("SDPBank.162@gmail.com");
+            msg.To.Add("serik.siti@gmail.com");
+            msg.Subject = "Example";
+            msg.Body = "Test";
+            SmtpClient client = new SmtpClient();
+            client.UseDefaultCredentials = true;
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Credentials = new NetworkCredential("SDPBank.162@gmail.com", "qwerty123*");
+            client.Timeout = 20000;
+            client.Send(msg);
+
+            Console.WriteLine("Message sended!");
+        }
+    }
+}
--- Messager/Program.cs
+++ Messager/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autofac;
 
 namespace Messager
 {
     class Program
     {
+        private static IContainer container { get; set; }
         static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<EmailSender>().As<IMessageSender>();
+            container = builder.Build();
+
+            var scope = container.BeginLifetimeScope();
+            var sender = scope.Resolve<IMessageSender>();
+
+            sender.SendMessage();
+
+            Console.ReadLine();
         }
     }
 }
